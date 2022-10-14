2bb3e0590451e1c6aae5b8967d5559ae49829fed
hoehlrich <hoehlrich@raleighcharterhs.org>
2022-10-14 14:02:33 -0400

    read patterns from file

--- src/lib.rs
+++ src/lib.rs
@@ -24,44 +24,51 @@ pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
 #[clap(author, version, about, trailing_var_arg = true)]
 pub struct Args {
     /// Pattern to search against
-    #[clap(required=true)]
+    #[clap(required = true)]
     pattern: String,
 
     /// Files to search
-    #[arg(required=true)]
+    #[arg(required = true)]
     files: Vec<String>,
 
     /// Use <PATTERNS> as the patterns. If used multiple times, search all patterns given
-    #[arg(long="regexp", short='e', group="Matching Control")]
+    #[arg(long = "regexp", short = 'e', group = "Matching Control")]
     patterns: Vec<String>,
 
+    /// Use <PATFILE> for patterns, one per line. Can be combined with <PATTERNS> and <PATTERN>
+    #[arg(long = "file", short = 'f', group = "Matching Control")]
+    pat_file: Vec<String>,
+
     /// Ignore case distinctions in patterns and input data
-    #[arg(short, long, group="Matching Control")]
+    #[arg(short, long, group = "Matching Control")]
     ignore_case: bool,
 
     /// Invert matching, to select non-matching lines
-    #[arg(short='v', long, group="Matching Control")]
+    #[arg(short = 'v', long, group = "Matching Control")]
     invert_match: bool,
 
     /// Select only lines containing matches that for whole words
-    #[arg(short='w', long, group="Matching Control")]
+    #[arg(short = 'w', long, group = "Matching Control")]
     word_regexp: bool,
 
     /// Select only matches that match the entire line
-    #[arg(short='x', long, group="Matching Control")]
+    #[arg(short = 'x', long, group = "Matching Control")]
     line_regexp: bool,
 }
 
 impl Args {
-    fn get_regexes(&self) -> Result<Vec<Regex>, regex::Error> {
-        let patterns = match self.patterns.len(){
-            0 => vec![self.pattern.clone()],
-            _ => {
-                let mut a: Vec<String> = self.patterns.clone();
-                a.push(self.pattern.clone());
-                a
-            },
-        };
+    fn get_regexes(&self) -> Result<Vec<Regex>, Box<dyn Error>> {
+        let mut patterns = vec![self.pattern.clone()];
+
+        // Add patterns from <PATTERNS>
+        let _ = self.patterns.iter().map(|p| patterns.push(p.clone()));
+
+        // Add patterns from <PATFILE>
+        for f in &self.pat_file {
+            for l in fs::read_to_string(f)?.lines() {
+                patterns.push(l.to_string());
+            }
+        }
 
         let mut regexes = vec![];
 
